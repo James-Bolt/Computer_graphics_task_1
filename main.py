30292cb96efa5e9d75129382d96f18bdb3fad3da
Miron Krivosheev <miron.student2001@bk.ru>
2021-10-13 18:53:55 +0300

    Часть функций

--- main.py
+++ main.py
@@ -2,8 +2,20 @@
 from classes import *
 
 
+def FindCountOfLectors(countOfSubjects):
+    return countOfSubjects / 3 + int(bool(countOfSubjects % 3))
+
+def CalculateSUMZPLectors(countOfLectors, salaryPerHour):
+    return (countOfLectors * 0.8 * 80 + countOfLectors * 0.2 * 40) * salaryPerHour * 12  # 80% работников работают на полную ставку, 20% на полвоину
+
+def RentCost(countOfStudents, rentCost, countOfLectors):
+    return rentCost * 4 * (countOfStudents + countOfLectors) * 1.05  # на каждого человека нужно 4 кв.м и +5% на тех. помещения
+
 
 if __name__ == '__main__':
-    x = University()
-    x.printMe()
+    Online_university = Online()
+    Offline_university = Offline()
+    FindCountOfLectors(Online_university.ourEducationPlan)
+
+
 
