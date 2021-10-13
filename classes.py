081b26794fec856a2c736e1c3f30b38daad744d2
Miron Krivosheev <miron.student2001@bk.ru>
2021-10-13 17:53:15 +0300

    Класс университета, изменение json

--- classes.py
+++ classes.py
@@ -2,8 +2,23 @@
 import json
 
 class University(object):
-    def __init__(self, input_data):
-        with open("input_university.json", "r") as read_file:
+    def __init__(self):
+        with open("base_university.json", "r") as read_file:
             data = json.load(read_file)
-            self.a = data['value']
+            self.countOfStudents = data['countOfStudents']
+            self.ourEducationPlan = data['ourEducationPlan']
+            self.internetCost = data['internetCost']
+            self.accountantSalary = data['accountantSalary']
+            self.cost1C = data['cost1C']
+            self.sysAdminSalary = data['sysAdminSalary']
+            self.methodistSalary = data['methodistSalary']
+            self.inflation = data['inflation']
+            self.durationOfEducation = data['durationOfEducation']
+
+class Offline(University):
+    def __init__(self):
+        super.__init__()
+        with open("offline_university.json", "r") as read_file:
+            data = json.load(read_file)
+            self.
 
