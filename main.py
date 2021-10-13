ce1b9f567e9ff86e171804d4940b741238158b27
James-Bolt <gleb.frolow2013@gmail.com>
2021-10-13 19:49:34 +0300

    продолжил делать main

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ def CountOfCleaners(sqare):
 
 def InventoryCost(countOfStudents):
     inventory = (countOfStudents/2 + 4) * 3400 + (countOfStudents + 10) * 2000  # на двух студентов 1 парта (стоимость 3400) + стулья(2000)
-    computers = (countOfStudents/2 + 2) * 60000  # на двух студентов 1 компьютер + 2 компьютера на кафедре
-    # 4 стола на всех преподавателей    10 стульев на всех преподавателей
+    computers = (countOfStudents/2 + 2) * 60000                                 # на двух студентов 1 компьютер + 2 компьютера на кафедре
+                                                                                # 4 стола на всех преподавателей    10 стульев на всех преподавателей
     return inventory + computers
 
 
@@ -47,9 +47,12 @@ if __name__ == '__main__':
     Offline_university = Offline()
 
     # Стоимость обучения онлайн
-    sumOnline = 0
     countOfLectors = FindCountOfLectors(Online_university.ourEducationPlan)
-    SUMZPLectors = CalculateSUMZPLectors(countOfLectors)
+    sUMZPLectors = CalculateSUMZPLectors(countOfLectors)
+    sqare = GeneralSqare(Online_university.countOfStudents, countOfLectors)
+    clouds = CloudRentCost(Online_university.cloudRent)
+    sumOnline = countOfLectors + sUMZPLectors + sqare + clouds + Online_university.internetCost
+
 
 
 
